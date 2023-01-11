18e8b383d45b6524bd87d8bfff62519e7c988e8c
rafaellaivnas <rafael.lavinas@korusconsultoria.com.br>
2023-01-11 09:07:24 -0300

    Atuailzação tratamento exceções

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExoApi", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
     {
